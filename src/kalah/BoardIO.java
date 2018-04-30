4a9bd94be47b79f7d5e3dc8052486d451c76e2a3
Max Griffith <mgriffith37@gmail.com>
2018-04-30 21:12:17 +1200

    Working prototype

---
+++ src/kalah/BoardIO.java
@@ -0,0 +1,50 @@
+package kalah;
+
+
+import com.qualitascorpus.testsupport.IO;
+import com.qualitascorpus.testsupport.MockIO;
+
+public class BoardIO {
+    private Board _board;
+    private IO _io;
+
+    public BoardIO(Board board, IO io){
+        _board = board;
+        _io = io;
+    }
+
+    public void printBoard(){
+        int[] currentBoardState = _board.getBoardState();
+        _io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+        _io.println(String.format("| P2 | 6[%2s] | 5[%2s] | 4[%2s] | 3[%2s] | 2[%2s] | 1[%2s] | %2s |",
+                currentBoardState[12], currentBoardState[11], currentBoardState[10], currentBoardState[9], currentBoardState[8], currentBoardState[7], currentBoardState[6]
+        ));
+        _io.println("|    |-------+-------+-------+-------+-------+-------|    |");
+        _io.println(String.format("| %2s | 1[%2s] | 2[%2s] | 3[%2s] | 4[%2s] | 5[%2s] | 6[%2s] | P1 |",
+                currentBoardState[13], currentBoardState[0], currentBoardState[1], currentBoardState[2], currentBoardState[3], currentBoardState[4], currentBoardState[5]
+        ));
+        _io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+    }
+
+    public int readPlayerPit(Player currentPlayer){
+       return  _io.readInteger(currentPlayer.id() + "'s turn - Specify house number or 'q' to quit: ", 1, 6, -1, "q");
+    }
+
+    public void gameOver(){
+        _io.println("Game over");
+    }
+
+    public void announceWinner(){
+        int p1Sum = _board.getTotal(Player.PLAYER1);
+        int p2Sum = _board.getTotal(Player.PLAYER2);
+        _io.println("\tplayer 1:" + p1Sum);
+        _io.println("\tplayer 2:" + p2Sum);
+
+        if (p1Sum == p2Sum) {
+            _io.println("A tie!");
+        } else {
+            _io.println(p1Sum > p2Sum ? "Player 1 wins!" : "Player 2 wins!");
+        }
+    }
+
+}
--- src/kalah/Kalah.java
+++ src/kalah/Kalah.java
@@ -16,76 +16,43 @@ public class Kalah {
 
 		Board gameBoard = new Board();
 		Player currentPlayer = Player.PLAYER1;
-		int[] currentBoardState = gameBoard.getBoardState();
 
-		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-		io.println(String.format("| P2 | 6[%2s] | 5[%2s] | 4[%2s] | 3[%2s] | 2[%2s] | 1[%2s] | %2s |",
-				currentBoardState[12], currentBoardState[11],currentBoardState[10], currentBoardState[9], currentBoardState[8], currentBoardState[7], currentBoardState[6]
-		));
-		io.println("|    |-------+-------+-------+-------+-------+-------|    |");
-		io.println(String.format("| %2s | 1[%2s] | 2[%2s] | 3[%2s] | 4[%2s] | 5[%2s] | 6[%2s] | P1 |",
-				currentBoardState[13], currentBoardState[0],currentBoardState[1], currentBoardState[2], currentBoardState[3], currentBoardState[4], currentBoardState[5]
-		));
-		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+		BoardIO boardIO = new BoardIO(gameBoard, io);
+		boardIO.printBoard();
 
-		while(gameBoard.isInPlay(currentPlayer)){
+		while (gameBoard.isInPlay(currentPlayer)) {
 
+			int houseNumber = boardIO.readPlayerPit(currentPlayer);
 
-
-			int houseNumber = io.readInteger(currentPlayer.id() + "'s turn - Specify house number or 'q' to quit: ", 1, 6, -1, "q");
-
-			if(houseNumber<0){
+			if (houseNumber < 0) {
 				break;
 			}
-			MoveOutcome outcome = MoveOutcome.Normal;
-			try{
-				outcome = gameBoard.makeMove(currentPlayer,  houseNumber);
-			} catch (Exception e){
+
+			MoveOutcome outcome;
+			try {
+				outcome = gameBoard.makeMove(currentPlayer, houseNumber);
+			} catch (Exception e) {
 				io.println("House is empty. Move again.");
 				outcome = MoveOutcome.RepeatTurn;
 			}
-			switch(outcome){
+
+			switch (outcome) {
 				case Normal:
 					currentPlayer = (currentPlayer == Player.PLAYER1) ? Player.PLAYER2 : Player.PLAYER1;
 					break;
 				case RepeatTurn:
 					break;
 			}
-			currentBoardState = gameBoard.getBoardState();
 
-			io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-			io.println(String.format("| P2 | 6[%2s] | 5[%2s] | 4[%2s] | 3[%2s] | 2[%2s] | 1[%2s] | %2s |",
-					currentBoardState[12], currentBoardState[11],currentBoardState[10], currentBoardState[9], currentBoardState[8], currentBoardState[7], currentBoardState[6]
-			));
-			io.println("|    |-------+-------+-------+-------+-------+-------|    |");
-			io.println(String.format("| %2s | 1[%2s] | 2[%2s] | 3[%2s] | 4[%2s] | 5[%2s] | 6[%2s] | P1 |",
-					currentBoardState[13], currentBoardState[0],currentBoardState[1], currentBoardState[2], currentBoardState[3], currentBoardState[4], currentBoardState[5]
-			));
-			io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+			boardIO.printBoard();
 
 		}
 
-		io.println("Game over");
+		boardIO.gameOver();
+		boardIO.printBoard();
 
-		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-		io.println(String.format("| P2 | 6[%2s] | 5[%2s] | 4[%2s] | 3[%2s] | 2[%2s] | 1[%2s] | %2s |",
-				currentBoardState[12], currentBoardState[11],currentBoardState[10], currentBoardState[9], currentBoardState[8], currentBoardState[7], currentBoardState[6]
-		));
-		io.println("|    |-------+-------+-------+-------+-------+-------|    |");
-		io.println(String.format("| %2s | 1[%2s] | 2[%2s] | 3[%2s] | 4[%2s] | 5[%2s] | 6[%2s] | P1 |",
-				currentBoardState[13], currentBoardState[0],currentBoardState[1], currentBoardState[2], currentBoardState[3], currentBoardState[4], currentBoardState[5]
-		));
-		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-
-		int p1Sum = gameBoard.getTotal(Player.PLAYER1);
-		int p2Sum = gameBoard.getTotal(Player.PLAYER2);
-		io.println( "\tplayer 1:" + p1Sum);
-		io.println("\tplayer 2:"+ p2Sum);
-		if(p1Sum == p2Sum){
-			io.println("A tie!");
-		}else {
-			io.println(p1Sum > p2Sum ? "Player 1 wins!" : "Player 2 wins!");
+		if (!gameBoard.isInPlay(currentPlayer)) {
+			boardIO.announceWinner();
 		}
 	}
-
 }
